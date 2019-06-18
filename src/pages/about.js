04e8bbd3b74643292fa279d8ceade278e464006d
azadeh <azad6026@gmail.com>
2019-06-18 21:27:22 +1000

    Added content to pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
-import Heading1 from "../components/headings/heading1"
+import Image from "../components/image"
 import SEO from "../components/seo"
 import Paragraph from "../components/paragraph"
+import Heading1 from "../components/headings/heading1"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About Us" />
-    <Heading1 class="centred">About Us</Heading1>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <section className="top-banner">
+      <Image filename="house-inblue-aircon.jpg" alt="some image" />
+      {/* <Paragraph className="banner-caption">ABOUT AIRCOLD</Paragraph> */}
+    </section>
+    <br />
+    <Heading1>About AIRCOLD</Heading1>
     <Paragraph>
       B N is a very passionate businessman that has been in his professional
       career for ove 20 years. He started the business on 2009 and has been
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,17 +1,59 @@
 import React from "react"
 import { Link } from "gatsby"
 import Heading1 from "../components/headings/heading1"
-
+import Heading2 from "../components/headings/heading2"
+import Heading3 from "../components/headings/heading3"
 import Layout from "../components/layout"
-import Pragraph from "../components/paragraph"
+import Paragraph from "../components/paragraph"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const ServicesPage = () => (
   <Layout>
     <SEO title="Services" />
-    <h1 style={{ textAlign: "center", margin: "2rem auto" }}>Our Services</h1>
-    <Pragraph>Here is a list of our services</Pragraph>
+    <section className="top-banner">
+      <Image
+        filename="homemain-banner-warm-house-with-split-unit.jpg"
+        alt="some image"
+      />
+      <br />
+    </section>
+    <section className="row-wrapper full-width services-page">
+      <h1 className="services-header">Our Services</h1>
+
+      <div className="inner-wrapper">
+        <article className="service-box">
+          <Image filename="different-solit-types.jpg" alt="some image" />
+          <aside>
+            <Heading3 class="main-title">Installation</Heading3>
+            <Paragraph>
+              Old or new, heating or cooling systems, both domestic and
+              commercial environments
+            </Paragraph>
+          </aside>
+        </article>
+        <article className="service-box">
+          <Image filename="old-conditioning-systems.jpg" alt="some image" />
+          <aside>
+            <Heading3 class="main-title">Maintenance</Heading3>
+            <Paragraph>
+              Practical maintenance / repairs for all heating and cooling
+              systems giving best
+            </Paragraph>
+          </aside>
+        </article>
+        <article className="service-box">
+          <Image filename="modern-splits-look.jpg" alt="some image" />
+          <aside>
+            <Heading3 class="main-title">Renewal</Heading3>
+            <Paragraph>
+              No matter how old is your conditioning system, we will provide
+              best solutions to .
+            </Paragraph>
+          </aside>
+        </article>
+      </div>
+    </section>
   </Layout>
 )
 
